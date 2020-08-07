f3fdf31a7d88049daae9f2aba400a3ff28b43ff2
MattDalla <43580489+MattDalla@users.noreply.github.com>
2020-08-07 02:38:45 +0200

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,7 +1,7 @@
 
 def notification(actual_url, final_url):
     if actual_url is not final_url:
-        '''SEND NOTIFICATION, NEW URL AQUIRED'''
+        '''SEND NOTIFICATION, NEW URL ACQUIRED'''
         return
     else:
         pass
